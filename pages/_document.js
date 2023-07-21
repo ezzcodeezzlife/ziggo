67dc1b253547852f8b9be9647c10c35822951c30
fabi.s <trapffm@web.de>
2023-07-21 22:39:10 +0200

    add main page

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className='scroll-smooth'>
         <Head>
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
           <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.Default.css" />
---
+++ pages/karte.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
+
+// Skip SSR for Leaflet, which has a dependency on `window`
+const DynamicMap = dynamic(() => import('../components/mapComponent.js'), {
+  ssr: false,
+});
+
+function HomePage() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <DynamicMap />
+    </div>
+  );
+}
+
+export default HomePage;
