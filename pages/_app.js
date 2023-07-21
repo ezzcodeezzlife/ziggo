8e995aa276ae313c67ccbede81c5c368dd94dd9c
fabi.s <trapffm@web.de>
2023-07-21 23:26:53 +0200

    add analticy code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
-
+import Script
+ from "next/script";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  
+
+
+  return  <><Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-LDCLSV0XN9"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LDCLSV0XN9');
+  `}
+</Script> <Component {...pageProps} /></>;
 }
