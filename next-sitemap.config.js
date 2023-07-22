2c4864dbe6e5e3c125a3a001b8d3c906e0a346e4
fabi.s <trapffm@web.de>
2023-07-22 17:35:44 +0200

    chaneg sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://zigarettenautomat-karte.vercel.app/',
+    siteUrl: process.env.SITE_URL || 'https://zigarettenautomatkarte.de/',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
