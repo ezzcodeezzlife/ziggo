c7abb4594878b501eaca17eed24746b4cd4ae3bb
fabi.s <trapffm@web.de>
2023-07-22 17:21:44 +0200

    add back home button

--- components/mapComponent.js
+++ components/mapComponent.js
@@ -152,8 +152,8 @@ const MapComponent = () => {
   return (
     <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
       <div id="map" style={{ width: '100%', height: '100%' }}></div>
-      <div style={{ position: 'absolute', bottom: '20px', left: '20px', zIndex: 1000 }}>
-        <a href="/" style={{ backgroundColor: 'white', padding: '10px', borderRadius: '5px', textDecoration: 'none', color: 'black' }}>← Homepage</a>
+      <div style={{ position: 'absolute', bottom: '10px', left: '10px', zIndex: 1000 }}>
+        <a href="/" style={{ backgroundColor: 'white', padding: '5px', borderRadius: '5px', textDecoration: 'none', color: 'black' }}>← Homepage</a>
       </div>
     </div>
   );
